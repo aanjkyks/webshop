db62a3de64ed56b1710d80384e01ca862b83874f
ainis.antans <ainis.antans@gmail.com>
2019-06-13 13:40:59 +0300

    Product object tests reformat and additional tests

--- src/test/java/com/webshop/internship/model/ProductTest.java
+++ src/test/java/com/webshop/internship/model/ProductTest.java
@@ -1,28 +1,41 @@
 package com.webshop.internship.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ProductTest {
-    @Test
-    public void productTest() {
-        Product testProduct = new Product();
+    private Product testProduct = new Product();
+
+    @Before
+    public void setUpTestProduct() {
         testProduct.setId(1L);
         testProduct.setName("name");
         testProduct.setPictureUrl("url");
         testProduct.setPrice(20.0);
+    }
 
+    @Test
+    public void productTestGettersSetters() {
         assertEquals(java.util.Optional.of(1L), java.util.Optional.ofNullable(testProduct.getId()));
         assertEquals("name", testProduct.getName());
         assertEquals("url", testProduct.getPictureUrl());
         assertEquals(java.util.Optional.of(20.0), java.util.Optional.ofNullable(testProduct.getPrice()));
+    }
 
+    @Test
+    public void productTestConstructorEquals() {
         Product anotherTestProduct = new Product(testProduct.getId(), testProduct.getName(), testProduct.getPrice(),
                 testProduct.getPictureUrl());
         assertEquals(testProduct, anotherTestProduct);
+    }
 
-
+    @Test
+    public void productTestHashCode() {
+        Product anotherTestProduct = new Product(testProduct.getId(), testProduct.getName(), testProduct.getPrice(),
+                testProduct.getPictureUrl());
+        assertEquals(testProduct.hashCode(), anotherTestProduct.hashCode());
     }
 
 }
