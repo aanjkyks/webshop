a53045e23979d5f8ca902c5af73fd32fb782856b
ainis.antans <ainis.antans@gmail.com>
2019-06-18 11:19:40 +0300

    OrderProduct DTO

---
+++ src/main/java/com/webshop/internship/dto/OrderProductDTO.java
@@ -0,0 +1,25 @@
+package com.webshop.internship.dto;
+
+import com.webshop.internship.model.Product;
+
+public class OrderProductDTO {
+
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
